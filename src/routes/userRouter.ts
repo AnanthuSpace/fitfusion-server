4b22350726e3668d69dc2157db841e93978f354e
Ananthu Mohan <ananthumohan368@gmail.com>
2024-07-28 22:23:40 +0530

    Complete the authentication without JWT

--- src/routes/userRouter.ts
+++ src/routes/userRouter.ts
@@ -5,5 +5,9 @@ const router = Router()
 const userController = new UserController()
 
 router.post("/signup", userController.registerController);
+router.post("/verify", userController.otpVerification);
+router.post("/login", userController.userLogin);
+router.post("/login-verify", userController.loginVerify);
+
 
 export default router

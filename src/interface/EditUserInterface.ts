2b5f01b31c3964e203ec62400c722d32e10ddd75
Ananthu Mohan <ananthumohan368@gmail.com>
2024-08-03 19:06:16 +0530

    User profile edited and extracted the userId

---
+++ src/interface/EditUserInterface.ts
@@ -0,0 +1,6 @@
+export interface EditUserInterface {
+    name: string;
+    phone: string;
+    address: string;
+    gender: string;
+}
--- src/repositories/userRepository.ts
+++ src/repositories/userRepository.ts
@@ -1,4 +1,5 @@
 import { UserType, userModel } from "../models/userModel";
+import { EditUserInterface } from "../interface/EditUserInterface";
 
 export class UserRepository {
 
@@ -8,5 +9,19 @@ export class UserRepository {
 
     async registerUser(userData: UserType){
         return await userModel.create(userData)
+    }
+
+    async editUser(editUserData: EditUserInterface, userId: string){
+        console.log("EditRepo");
+        
+        return await userModel.updateOne({userId}, {$set: editUserData})
+    }
+
+    async changePass(newPassword: string, userId:string){
+        return await userModel.updateOne({userId},{$set: {password: newPassword}})
+    }
+
+    async findEditingData(userId:string) {
+        return await userModel.findOne({ userId: userId })
     }
 }
